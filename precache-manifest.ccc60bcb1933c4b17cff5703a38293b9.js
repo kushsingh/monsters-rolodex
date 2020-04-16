fe7c754f30a21e4e5e921d2f2c5a024546c3032d
Kush Singh <kush.singh84@gmail.com>
2020-04-17 00:10:33 +0530

    Updates

---
+++ precache-manifest.ccc60bcb1933c4b17cff5703a38293b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a1cd2a5ee3c242112fab9bbe8186d0",
+    "url": "/mono-rolo/index.html"
+  },
+  {
+    "revision": "827a90d231446447f182",
+    "url": "/mono-rolo/static/css/main.394554ba.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/mono-rolo/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mono-rolo/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "827a90d231446447f182",
+    "url": "/mono-rolo/static/js/main.b64fc840.chunk.js"
+  },
+  {
+    "revision": "f11635c7cfd892f65a06",
+    "url": "/mono-rolo/static/js/runtime-main.a5af2b21.js"
+  }
+]);
